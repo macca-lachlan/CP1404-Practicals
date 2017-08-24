28b7d169230ef22b1c089196f1537da552c71569
macca-lachlan <30332634+macca-lachlan@users.noreply.github.com>
2017-08-24 12:01:15 +1000

    remove redundant line

--- prac_04/lottery_ticket_generator.py
+++ prac_04/lottery_ticket_generator.py
@@ -5,8 +5,7 @@ def main():
 
     number_of_quick_picks = int(input("how many quick picks do you wish to generate? :"))
     for i in range(1, number_of_quick_picks + 1):
-        numbers = []
-        numbers.append(random.randint(1, 45))
+        numbers = [random.randint(1, 45)]
         while len(numbers) < 6:
             number = random.randint(1, 45)
             if number not in numbers:
