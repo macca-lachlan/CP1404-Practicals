39587db2cf06d280eeb761a5628c91449bd247fd
macca-lachlan <30332634+macca-lachlan@users.noreply.github.com>
2017-10-05 11:10:43 +1000

    complete walkthrough example

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,15 @@
+from prac_08.taxi import Taxi
+
+
+def main():
+    prius = Taxi("Prius 1", 100)
+    prius.drive(40)
+    print(prius)
+    print(prius.get_fare())
+    prius.start_fare()
+    prius.drive(100)
+    print(prius)
+    print(prius.get_fare())
+
+
+main()
